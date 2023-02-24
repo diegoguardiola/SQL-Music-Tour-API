de876d886ddb3a11694c9e59f0b859d9a2bb2323
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-02-23 21:43:35 -0500

    added band using postman

---
+++ migrations/20230224014852-create-set-time.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('settimes', {
+      set_time_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      set_time_id: {
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.INTEGER
+      },
+      stage_id: {
+        type: Sequelize.INTEGER
+      },
+      band_id: {
+        type: Sequelize.INTEGER
+      },
+      start_time: {
+        type: Sequelize.DATE
+      },
+      end_time: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('SetTimes');
+  }
+};
