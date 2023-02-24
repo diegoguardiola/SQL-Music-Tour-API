c7fd6bc85aa4779629e6431a2c127797bfeefd21
Diego Guardiola <diego.guardiola@falcoautomation.local>
2023-02-23 19:45:48 -0500

    db migrate

--- server.js
+++ server.js
@@ -9,22 +9,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: process.env.PG_PASSWORD
-  })
-  // SEQUELIZE CONNECTION
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
-  
 
 
 // ROOT
